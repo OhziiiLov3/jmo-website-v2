8536a7f7a15a379d7b20606f90350db23d9dc172
ohziiilov3 <klbaskerville0520@gmail.com>
2022-11-30 16:36:57 -0800

    feat:add hero component and css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div >
      <Navbar/>
+     <Hero/>
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="content">
+            <p>Email to Book</p>
+            <p>joinmyobession@gmail.com</p>
+            <p>JoinMyObession</p>
+            <p>Get in Shape & Feel Good</p>
+            <button href='/' className='button'>Free Consultation</button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
